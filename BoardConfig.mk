8d0f9953ec96a86369bb2a5f84d6539d6b82d5e3
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-03-17 08:46:21 +0900

    Initial import.
    Kernel in stock rom is used for now for the safety.

---
+++ BoardConfig.mk
@@ -0,0 +1,32 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/sharp/is17sh/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_FPU : = neon
+TARGET_BOOTLOADER_BOARD_NAME := is17sh
+
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/sharp/is17sh/recovery/keys.c
+
+BOARD_KERNEL_CMDLINE := console=ttyDCC0 androidboot.hardware=qcom
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_PAGESIZE := 4096
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/sharp/is17sh/kernel
+
+#BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+#BOARD_HAS_LARGE_FILESYSTEM := true
---
+++ device_is17sh.mk
@@ -0,0 +1,28 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/sharp/is17sh/is17sh-vendor.mk)
+
+PRODUCT_COPY_FILES += \
+  device/sharp/is17sh/system/SH_touchpanel.idc:system/usr/idc/SH_touchpanel.idc \
+  device/sharp/is17sh/system/shtps_rmi.idc:system/usr/idc/shtps_rmi.idc
+
+DEVICE_PACKAGE_OVERLAYS += device/sharp/is17sh/overlay
+
+LOCAL_PATH := device/sharp/is17sh
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_is17sh
+PRODUCT_DEVICE := is17sh
