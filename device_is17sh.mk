e3bc9798bdaeb456254c0280c5d4351739b65c6a
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-03-17 16:44:21 +0900

    Build kernel.

--- device_is17sh.mk
+++ device_is17sh.mk
@@ -11,16 +11,6 @@ PRODUCT_COPY_FILES += \
 
 DEVICE_PACKAGE_OVERLAYS += device/sharp/is17sh/overlay
 
-LOCAL_PATH := device/sharp/is17sh
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
